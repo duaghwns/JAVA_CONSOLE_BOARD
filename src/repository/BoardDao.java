31dc78948de595f34c6915eca41877362f87ad2c
duaghwns <hj2758@gmail.com>
2022-08-27 21:31:50 +0900

    이름변경

--- src/repository/BoardDao.java
+++ src/repository/BoardDao.java
@@ -4,32 +4,13 @@ import entity.Board;
 
 import java.util.List;
 
-public class BoardDao implements BoardRepogitory{
+public interface BoardDao {
 
-    private static BoardRepogitory repogitory;
+    Board read(long id);
 
-    public BoardDao() {
+    int insert(Board bd);
 
-    }
+    int update(Board bd);
 
-    @Override
-    public Board read(long id) {
-
-        return null;
-    }
-
-    @Override
-    public int insert(Board bd) {
-        return 0;
-    }
-
-    @Override
-    public int update(Board bd) {
-        return 0;
-    }
-
-    @Override
-    public List<Board> searchBoard() {
-        return null;
-    }
+    List<Board> searchBoard();
 }
