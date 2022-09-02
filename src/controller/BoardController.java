f14eccdefa5dd839fba41c4e3fbfe55822fdcf73
duaghwns <hj2758@gmail.com>
2022-09-02 14:26:35 +0900

    board

--- src/controller/BoardController.java
+++ src/controller/BoardController.java
@@ -7,7 +7,6 @@ import vo.BoardVO;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Scanner;
 
 public class BoardController {
     DataBase db = DataBase.getInstance();
