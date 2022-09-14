0d275a10ea02b7076aa09190f2dd834ef0e1fefe
duaghwns <hj2758@gmail.com>
2022-09-14 17:37:56 +0900

    JAVA CONSOLE BOARD

--- src/BoardStart.java
+++ src/BoardStart.java
@@ -4,9 +4,7 @@ import java.io.IOException;
 
 public class BoardStart {
     public static void main(String[] args) throws IOException {
-        System.out.println("------------------------------");
-        System.out.println("-------------게시판-------------");
-        System.out.println("------------------------------");
+        System.out.println("=======================JAVA CONSOLE BOARD=======================");
         new BoardController().mainView();
     }
 }
